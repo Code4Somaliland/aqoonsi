699a49d44c9657c80ddd3d176bf5573807c10b78
Haadka <Haadka@gmail.com>
2021-05-08 23:23:40 +1000

    test pipline

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Aqoonsi.com</h1>
+<p>
+	Aqoonsi means recognition in Somail language. Aqoonsi.com is a list of all World leaders and
+	interntioan experts who have recognies Somaliland as a country or even more.
+</p>
