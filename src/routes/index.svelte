74a4dec2e80893985355f4cee6af5c325c34d984
Haadka <Haadka@gmail.com>
2021-05-08 23:31:31 +1000

    typos

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <h1>Welcome to Aqoonsi.com</h1>
 <p>
-	Aqoonsi means recognition in Somail language. Aqoonsi.com is a list of all World leaders and
-	interntioan experts who have recognies Somaliland as a country or even more.
+	Aqoonsi means recognition in the Somali language. Aqoonsi.com is a list of all World leaders and
+	international experts who have recognised Somaliland as a country or even more.
 </p>
