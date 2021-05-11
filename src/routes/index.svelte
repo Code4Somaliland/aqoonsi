342e9b3b1fe5b449871e2602dc14711524a54ba2
Haadka <Haadka@gmail.com>
2021-05-12 00:40:38 +1000

    theme and first content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,186 @@
-<h1>Welcome to Aqoonsi.com</h1>
-<p>
-	Aqoonsi means recognition in the Somali language. Aqoonsi.com is a list of all World leaders and
-	international experts who have recognised Somaliland as a country or even more.
-</p>
+<!-- Slide 1 (#04) -->
+<section class="slide fade-6 kenBurns">
+	<div class="content">
+		<div class="container">
+			<div class="wrap">
+				<div class="fix-5-12">
+					<div class="fix-3-12 margin-bottom-5">
+						<h1>Bill Gates</h1>
+					</div>
+					<h4 class="margin-bottom-5">"Somaliland is very small. But it is phenomenal".</h4>
+					<h4 class="margin-bottom-5">
+						"if there is one emerging economy that India should look at, it is Somaliland".
+					</h4>
+
+					<div class="videoThumbnail shadow rounded ae-4 fromCenter popupTrigger" data-popup-id="4">
+						<img src="assets/img/forground/bill-gates.jpeg" alt="Video Thumbnail" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="background" style="background-image:url(assets/img/background/clouds.jpeg)" />
+</section>
+
+<!-- Popup Video -->
+<div class="popup autoplay" data-popup-id="4">
+	<div class="close">
+		<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#close" /></svg>
+	</div>
+	<div class="content">
+		<div class="container">
+			<div class="wrap">
+				<div class="fix-10-12">
+					<div class="embedVideo popupContent shadow rounded">
+						<iframe
+							data-src="https://www.youtube.com/embed/azAebCFoJ80?start=142"
+							frameborder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Slide 2 (#04) -->
+<section class="slide fade-6 kenBurns">
+	<div class="content">
+		<div class="container">
+			<div class="wrap">
+				<div class="fix-6-12">
+					<div class="fix-6-12 margin-bottom-5">
+						<h2>Somaliland deserves international recognition</h2>
+					</div>
+					<h4 class="margin-bottom-5">"To recognise that is to recognise reality".</h4>
+					<div>
+						<a
+							href="https://www.economist.com/leaders/2021/05/08/somaliland-deserves-international-recognition"
+						>
+							<img src="assets/img/forground/the-economist.png" alt="the-economist" />
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="background" style="background-image:url(assets/img/background/ocean-dark.jpeg)" />
+</section>
+
+<!-- Slide 3 (#04) -->
+<section class="slide fade-6 kenBurns">
+	<div class="content">
+		<div class="container">
+			<div class="wrap">
+				<div class="fix-6-12">
+					<div class="fix-6-12 margin-bottom-5">
+						<h2>President Tsai Ing Wen</h2>
+					</div>
+					<h4 class="margin-bottom-5">
+						"Somaliland and Taiwan are bonded by shared democratic values and freedoms".
+					</h4>
+					<div>
+						<a href="https://english.president.gov.tw/News/6052">
+							<img src="assets/img/forground/tsai.jpeg" alt="Tsai Ing Wen" />
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="background" style="background-image:url(assets/img/background/beams.jpeg)" />
+</section>
+
+<!-- Slide 4 (#04) -->
+<section class="slide fade-6 kenBurns">
+	<div class="content">
+		<div class="container">
+			<div class="wrap">
+				<div class="fix-6-12">
+					<div class="fix-6-12 margin-bottom-5">
+						<h2>Jendayi Frazer</h2>
+					</div>
+					<h4 class="margin-bottom-5">
+						"One of my regrest as assistant secretary was that I didn't push hard enough for us to
+						recognize Somaliland. I think we should have".
+					</h4>
+					<div class="videoThumbnail shadow rounded ae-4 fromCenter popupTrigger" data-popup-id="2">
+						<img src="assets/img/forground/jendayi.jpeg" alt="Video Thumbnail" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="background" style="background-image:url(assets/img/background/beams.jpeg)" />
+</section>
+<!-- Popup Video -->
+<div class="popup autoplay" data-popup-id="2">
+	<div class="close">
+		<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#close" /></svg>
+	</div>
+	<div class="content">
+		<div class="container">
+			<div class="wrap">
+				<div class="fix-10-12">
+					<div class="embedVideo popupContent shadow rounded">
+						<iframe
+							data-src="https://www.youtube.com/embed/ggy6iTrwRfc"
+							frameborder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Slide 5 (#04) -->
+<section class="slide fade-6 kenBurns">
+	<div class="content">
+		<div class="container">
+			<div class="wrap">
+				<div class="fix-6-12">
+					<div class="fix-6-12 margin-bottom-5">
+						<h2>Dr. James Ker-Lindsay</h2>
+					</div>
+					<h4 class="margin-bottom-5">
+						"Somaliland deseves recognition more than any other de facto country".
+					</h4>
+					<h4 class="margin-bottom-5">
+						"I can see Somaliland as a member of UN but it needs one country to take first step".
+					</h4>
+					<div class="videoThumbnail shadow rounded ae-4 fromCenter popupTrigger" data-popup-id="3">
+						<img src="assets/img/forground/james.jpeg" alt="Video Thumbnail" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="background" style="background-image:url(assets/img/background/sand.jpeg)" />
+</section>
+<!-- Popup Video -->
+<div class="popup autoplay" data-popup-id="3">
+	<div class="close">
+		<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#close" /></svg>
+	</div>
+	<div class="content">
+		<div class="container">
+			<div class="wrap">
+				<div class="fix-10-12">
+					<div class="embedVideo popupContent shadow rounded">
+						<iframe
+							data-src="https://www.youtube.com/embed/yc9-aMlo-SI?start=245"
+							frameborder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
