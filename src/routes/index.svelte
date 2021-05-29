f2588746155fc1fbaf567b09fd8b7db5b6513e9f
Haadka <Haadka@gmail.com>
2021-05-30 03:39:47 +1000

    add tibor and iqbal

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,7 +12,7 @@
 						"if there is one emerging economy that India should look at, it is Somaliland".
 					</h4>
 
-					<div class="videoThumbnail shadow rounded ae-4 fromCenter popupTrigger" data-popup-id="4">
+					<div class="videoThumbnail shadow rounded ae-4 fromCenter popupTrigger" data-popup-id="1">
 						<img src="assets/img/forground/bill-gates.jpeg" alt="Video Thumbnail" />
 					</div>
 				</div>
@@ -23,7 +23,7 @@
 </section>
 
 <!-- Popup Video -->
-<div class="popup autoplay" data-popup-id="4">
+<div class="popup autoplay" data-popup-id="1">
 	<div class="close">
 		<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#close" /></svg>
 	</div>
@@ -182,3 +182,73 @@
 		</div>
 	</div>
 </div>
+
+<!-- Slide 6 (#04) -->
+<section class="slide fade-6 kenBurns">
+	<div class="content">
+		<div class="container">
+			<div class="wrap">
+				<div class="fix-6-12">
+					<div class="fix-6-12 margin-bottom-2">
+						<h2>Ambassador Prof Iqbal Jhazbhay</h2>
+					</div>
+					<h4 class="margin-bottom-2">"Somaliland has a very good case in international law".</h4>
+					<div class="videoThumbnail shadow rounded ae-4 fromCenter popupTrigger" data-popup-id="4">
+						<img src="assets/img/forground/iqbal.jpeg" alt="Video Thumbnail" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="background" style="background-image:url(assets/img/background/sand.jpeg)" />
+</section>
+<!-- Popup Video -->
+<div class="popup autoplay" data-popup-id="4">
+	<div class="close">
+		<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#close" /></svg>
+	</div>
+	<div class="content">
+		<div class="container">
+			<div class="wrap">
+				<div class="fix-10-12">
+					<div class="embedVideo popupContent shadow rounded">
+						<iframe
+							data-src="https://www.youtube.com/embed/UqaE6ldUems?start=253"
+							frameborder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Slide 7 (#04) -->
+<section class="slide fade-6 kenBurns">
+	<div class="content">
+		<div class="container">
+			<div class="wrap">
+				<div class="fix-6-12">
+					<div class="fix-6-12 margin-bottom-5">
+						<h2>A. Secretary of State for Africa</h2>
+						<h2>Tibor Nagy</h2>
+					</div>
+					<h4 class="margin-bottom-2">
+						"The US should definitely look at a formal relationship with Somaliland because it's a
+						state that works"
+					</h4>
+					<div>
+						<a target="_blank" href="https://twitter.com/Somaliland_Now/status/1394666704714747904">
+							<div class="videoThumbnail shadow rounded">
+								<img src="assets/img/forground/tibor.jpeg" alt="Tibor Nagy" />
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="background" style="background-image:url(assets/img/background/beams.jpeg)" />
+</section>
